cbbaa7ae6db32755246d4349911b5404d95c2d3c
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-23 22:41:37 +0300

    cs

---
+++ ProjeRegisterForm/ProjeRegisterForm/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeRegisterForm.Models.Entity;
+namespace ProjeRegisterForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        DbMvcRegisterFormEntities db = new DbMvcRegisterFormEntities();
+        public ActionResult AddStudent(TBL_STUDENT p)
+        {
+            db.TBL_STUDENT.Add(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ ProjeRegisterForm/ProjeRegisterForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjeRegisterForm.MvcApplication" Language="C#" %>
