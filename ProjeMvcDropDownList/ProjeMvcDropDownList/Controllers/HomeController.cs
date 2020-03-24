c93473e0093a5046ac276c175d5335d54708a96a
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-24 18:00:34 +0300

    cs

---
+++ ProjeMvcDropDownList/ProjeMvcDropDownList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeMvcDropDownList.Models.Entity;
+namespace ProjeMvcDropDownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbMvcCityEntities db = new DbMvcCityEntities();
+        public ActionResult Index()
+        {
+            return View(db.TBL_CITY.ToList());
+        }
+
+
+    }
+}
